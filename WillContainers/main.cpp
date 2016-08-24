acdc50c6da86d3df8d92ae11bc264300e88f47d2
Julien Esposito <julien.esposito@gmail.com>
2016-08-24 12:14:49 -0700

    Initial Commit

---
+++ WillContainers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WillContainers
+//
+//  Created by Julien Esposito on 8/24/16.
+//  Copyright © 2016 Julien Esposito. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
